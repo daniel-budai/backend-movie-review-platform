e16fc2f1bd4a2be1ac654504eb8fbe59ee454cd6
daniel-budai <danielbudai@tutanota.com>
2024-05-31 07:33:04 +0200

    routes for user, reviews, movies

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const movieController = require("../controllers/movieController");
+
+router.get("/", movieController.getAllMovies);
+router.post("/", movieController.createMovie);
+router.get("/:id", movieController.getMovieById);
+router.put("/:id", movieController.updateMovieById);
+router.get("/movies/:id/reviews", movieController.getAllReviewsForMovie); //hämta för en specifik film
+router.delete("/movies/:id", movieController.deleteMovieById);
 
 module.exports = router;
